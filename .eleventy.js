65b4369be7716c9fa92868cb7d9e21d10feb0ede
path-96 <ymhtet96@gmail.com>
2022-04-21 05:56:43 +0900

    create basic templating with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  // Copy the `css` directory to the output
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('assets');
+
+
+  // Watch the `css` directory for changes
+  eleventyConfig.addWatchTarget('css');
+};
